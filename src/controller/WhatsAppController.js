70df7b4c3df1593ba49dbbe8a7f700e079d1b0f1
zYumoto <victorjuanrazer@gmail.com>
2022-10-10 17:29:17 -0300

    add: events on menu attach

--- src/controller/WhatsAppController.js
+++ src/controller/WhatsAppController.js
@@ -150,29 +150,86 @@ class WhatsAppController {
         });
 
         this.el.btnAttach.on("click",e=>{
+            e.stopPropagation();
             this.el.menuAttach.addClass("open");
-            document.addEventListener("click", this.closeMenuAttach);
+            document.addEventListener("click", this.closeMenuAttach.bind(this));
 
         });
 
         this.el.btnAttachPhoto.on("click", e=>{
-                console.log("photo");
+                this.el.inputPhoto.click();
         });
-        this.el.btnAttachCamera.on("click", e=>{
-            console.log("camera");
-        });
-        this.el.btnAttachDocument.on("click", e=>{
-            console.log("Document");
-        });
-        this.el.btnAttachContact.on("click", e=>{
-            console.log("Contact");
+        this.el.inputPhoto.on("change", e=>{
+                console.log(this.el.inputPhoto.files);
+
+
+                [...this.el.inputPhoto.files].forEach(file=>{
+                    console.log();
+                });
         });
 
+
+        this.el.btnAttachCamera.on("click", e=>{
+            console.log("camera");
+
+            this.closeAllMainPanel();
+            this.el.panelCamera.addClass("open");
+            this.el.panelCamera.css({
+                "heigth":"calc(100% - 120px)"
+            });
+
+        });
+
+        this.el.btnClosePanelCamera.on("click", e=>{
+            this.closeAllMainPanel();
+            this.el.panelMessagesContainer.show();
+
+       });
+
+       this.el.btnTakePicture.on("click",e =>{
+        console.log("take picture");
+       });
+
+
+        this.el.btnAttachDocument.on("click", e=>{
+            this.closeAllMainPanel();
+           this.el.panelDocumentPreview.addClass("open");
+                this.el.panelDocumentPreview.css({
+                "heigth":"calc(100% - 120px)"
+            });
+           
+        });
+
+        this.el.btnClosePanelDocumentPreview.on("click",e=>{
+            this.closeAllMainPanel();
+            this.el.panelMessagesContainer.show();
+        });
+
+        this.el.btnSendDocument.on("click",e=>{
+            console.log("send document");
+        });
+
+
+        this.el.btnAttachContact.on("click", e=>{
+            this.el.modalContacts.show();
+        });
+
+        this.el.btnCloseModalContacts.on("click",e=>{
+            this.el.modalContacts.hide();
+        });
 
     }
 
+        closeAllMainPanel(){
+            this.el.panelMessagesContainer.hide();  
+            this.el.panelDocumentPreview.removeClass("open");
+            this.el.panelCamera.removeClass("open");
+        }
+
+
     closeMenuAttach(e){
         document.removeEventListener("click",this.closeMenuAttach);
+        console.log(this); 
         this.el.menuAttach.removeClass("open");
         console.log("remove menu");
     }
