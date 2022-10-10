6f8e2ed66e05b0c380d589221bf365d09e7d146d
zYumoto <victorjuanrazer@gmail.com>
2022-10-10 18:04:30 -0300

    Add:milliseconds to minutes function

--- src/controller/WhatsAppController.js
+++ src/controller/WhatsAppController.js
@@ -243,7 +243,7 @@ class WhatsAppController {
 
             this._recordMicrophoneInterval = setInterval(()=>{
                 
-                this.el.recordMicrophoneTimer.innerHTML = (Date.now() - start);
+                this.el.recordMicrophoneTimer.innerHTML = Format.toTime(Date.now() - start);
             },100);
 
         }
