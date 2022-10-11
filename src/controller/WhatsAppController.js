3515fa38d7c9ae91b82dcc7dadcb63e9ae346c75
zYumoto <victorjuanrazer@gmail.com>
2022-10-11 09:05:58 -0300

    add: emojis and send emojis on chat

--- src/controller/WhatsAppController.js
+++ src/controller/WhatsAppController.js
@@ -233,8 +233,55 @@ class WhatsAppController {
         this.el.btnFinishlMicrophone.on("click", e=>{
             this.closeRecordMicrophone();
         });
+        this.el.inputText.on("keypress", e=>{
+            if(e.key === "Enter" && !e.ctrlKey){
+                e.preventDefault();
+                        this.el.btnSend.click();
+            }
+        });
+
+        this.el.inputText.on("keyup", e=>{
+
+            if(this.el.inputText.innerHTML.length){
+                this.el.inputPlaceholder.hide();
+                this.el.btnSendMicrophone.hide();
+                this.el.btnSend.show();
+            }
+            else{
+                this.el.inputPlaceholder.show();
+                this.el.btnSendMicrophone.show();
+                this.el.btnSend.hide();
+            }
+
+        });
+
+                this.el.btnSend.on("click", e=>{
+                    console.log(this.el.inputText.innerHTML);
+                });
 
 
+                this.el.btnEmojis.on("click", e=>{
+                    this.el.panelEmojis.toggleClass("open");
+                });
+
+
+                this.el.panelEmojis.querySelectorAll(".emojis").forEach(emoji=>{
+                    emoji.on("click", e=>{
+                        
+
+                        let img = this.el.imgEmojiDefault.cloneNode();
+
+                        img.style.cssText = emoji.style.cssText;
+                        img.dataset.unicode = emoji.dataset.unicode;
+                        img.alt = emoji.dataset.unicode;
+
+                        emoji.classList.forEach(name=>{
+                            img.classList.add(name);
+                        });
+                        this.el.inputText.appendChild(img);
+                        this.el.inputText.dispatchEvent(new Event ("keyup"));
+                    });
+                });
     }   
 
         startRecordMicrophoneTime(){
