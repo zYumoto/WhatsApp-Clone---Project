c7181a86bef1fa946c980b3590ba7629c19cb8e2
zYumoto <victorjuanrazer@gmail.com>
2022-10-18 13:57:10 -0300

    update: message status

--- src/controller/WhatsAppController.js
+++ src/controller/WhatsAppController.js
@@ -190,17 +190,34 @@ export class WhatsAppController {
                 let data = doc.data();
                 data.id = doc.id;
 
+                let message = new Message();
+
+                message.fromJSON(data);
+
+                let me = (data.from === this._user.email);
+
                  if (!this.el.panelMessagesContainer.querySelector('#_' + data.id)) {
 
-                    let message = new Message();
+                    if (!me) {
 
-                    message.fromJSON(data);
+                        doc.ref.set({
+                            status: 'read'
+                        }, {
+                            merge: true
+                        })
 
-                    let me = (data.from === this._user.email);
+                    }
 
                     let view = message.getViewElement(me);
 
                     this.el.panelMessagesContainer.appendChild(view);
+
+                } else if (me) {
+
+                    let msgEl = this.el.panelMessagesContainer.querySelector('#_' + data.id)
+
+                    msgEl.querySelector('.message-status').innerHTML = message.getStatusViewElement().outerHTML;
+
                 }
             });
 
